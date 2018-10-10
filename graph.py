80d9b0e39aa5e6875b05d6c4a0675eec16a5af0e
dongzeyuan <dongfujing88@gmail.com>
2018-10-10 22:41:47 +0800

    map

--- graph.py
+++ graph.py
@@ -2,11 +2,14 @@ import libtcodpy as tcod
 
 SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
+MAP_WIDTH = 80
+MAP_HEIGHT = 45
 LIMITS_FPS = 30
 player_x = (SCREEN_WIDTH // 2)
 player_y = (SCREEN_HEIGHT // 2)
 
-font_path = 'D:\code\BBT\\fonts\\arial10x10.png'
+# font_path = 'D:\code\BBT\\fonts\\arial10x10.png'
+font_path = 'E:\Python\\bbt\\fonts\\arial10x10.png'
 font_flags = tcod.FONT_TYPE_GREYSCALE | tcod.FONT_LAYOUT_TCOD
 tcod.console_set_custom_font(font_path, font_flags)
 
@@ -15,6 +18,25 @@ fullscreen = False
 tcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, window_title, fullscreen)
 con = tcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
 tcod.sys_set_fps(LIMITS_FPS)
+
+
+class Object(object):
+    def __init__(self, x, y, char, color):
+        self.x = x
+        self.y = y
+        self.char = char
+        self.color = color
+
+    def move(self, dx, dy):
+        self.x += dx
+        self.y += dy
+
+    def draw(self):
+        tcod.console_set_default_foreground(con, self.color)
+        tcod.console_put_char(con, self.x, self.y, self.char, tcod.BKGND_NONE)
+
+    def clear(self):
+        tcod.console_put_char(con, self.x, self.y, " ", tcod.BKGND_NONE)
 
 
 def handle_keys():
@@ -35,11 +57,19 @@ def handle_keys():
         player_x = player_x + 1
 
 
+player = Object(player_x, player_y, "@", tcod.white)
+npc = Object(player_x - 5, player_y, "@", tcod.yellow)
+objects = [player, npc]
+
+
 while not tcod.console_is_window_closed():
-    tcod.console_set_default_background(0, tcod.white)
-    tcod.console_put_char(0, player_x, player_y, '@', tcod.BKGND_NONE)
+    tcod.console_set_default_background(con, tcod.white)
+    for object in objects:
+        object.draw()
+        object.clear()
+
     tcod.console_flush()
-    tcod.console_put_char(0, player_x, player_y, ' ', tcod.BKGND_NONE)
+
     exit = handle_keys()
 
     if exit:
