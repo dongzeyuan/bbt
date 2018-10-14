cc4e1662a9e1fe4934951be4694bccb46376ff42
dongzeyuan <dongfujing88@gmail.com>
2018-10-14 22:16:12 +0800

    dungeon generator

--- map.py
+++ map.py
@@ -8,6 +8,11 @@ SCREEN_HEIGHT = 50
 MAP_WIDTH = 80
 MAP_HEIGHT = 45
 
+# number of the room
+ROOM_MAX_SIZE = 10
+ROOM_MIN_SIZE = 6
+MAX_ROOMS = 30
+
 LIMIT_FPS = 20  # 20 frames-per-second maximum
 
 
@@ -53,14 +58,47 @@ class Object:
 
 
 def make_map():
-    global game_map
+    global game_map, player
     # fill map with "unblocked" tiles
-    game_map = [[Tile(False) for y in range(SCREEN_HEIGHT)] for x in range(SCREEN_WIDTH)]
+    game_map = [[Tile(True) for y in range(SCREEN_HEIGHT)]
+                for x in range(SCREEN_WIDTH)]
 
-    room1 = Rect(20, 15, 10, 15)
-    room2 = Rect(50, 15, 10, 15)
-    creat_room(room1)
-    creat_room(room2)
+    rooms = []
+    num_rooms = 0
+    for r in range(MAX_ROOMS):
+        w = libtcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+        h = libtcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+
+        x = libtcod.random_get_int(0, 0, MAP_WIDTH - w - 1)
+        y = libtcod.random_get_int(0, 0, MAP_HEIGHT - h - 1)
+
+        new_room = Rect(x, y, w, h)
+        failed = False
+        for other_room in rooms:
+            if new_room.intersect(other_room):
+                failed = True
+                break
+        if not failed:
+            create_room(new_room)
+            (new_x, new_y) = new_room.center()
+
+            if num_rooms == 0:
+                player.x = new_x
+                player.y = new_y
+
+            else:
+                (prev_x, prev_y) = rooms[num_rooms - 1].center()
+
+                if libtcod.random_get_int(0, 0, 1) == 1:
+                    create_h_tunnel(prev_x, new_x, prev_y)
+                    create_v_tunnel(prev_y, new_y, new_x)
+
+                else:
+                    create_v_tunnel(prev_y, new_y, prev_x)
+                    create_h_tunnel(prev_x, new_x, new_y)
+
+            rooms.append(new_room)
+            num_rooms += 1
 
 
 class Rect():
@@ -70,13 +108,36 @@ class Rect():
         self.x2 = x + w
         self.y2 = y + h
 
+    def center(self):
+        center_x = (self.x1 + self.x2) // 2
+        center_y = (self.y1 + self.y2) // 2
+        return(center_x, center_y)
 
-def creat_room(room):
+    def intersect(self, other):
+        return (self.x1 <= other.x2 and self.x2 >= other.x1 and
+                self.y1 <= other.y2 and self.y2 >= other.y1)
+
+
+def create_room(room):
     global game_map
     for x in range(room.x1, room.x2 + 1):
         for y in range(room.y1, room.y2 + 1):
             game_map[x][y].blocked = False
             game_map[x][y].block_sight = False
+
+
+def create_h_tunnel(x1, x2, y):
+    global game_map
+    for x in range(min(x1, x2), max(x1, x2)+1):
+        game_map[x][y].blocked = False
+        game_map[x][y].block_sight = False
+
+
+def create_v_tunnel(y1, y2, x):
+    global game_map
+    for y in range(min(y1, y2), max(y1, y2) + 1):
+        game_map[x][y].blocked = False
+        game_map[x][y].block_sight = False
 
 
 def render_all():
@@ -149,6 +210,7 @@ objects = [player]
 
 # generate map (at this point it's not drawn to the screen)
 make_map()
+create_h_tunnel(25, 55, 23)
 
 
 while not libtcod.console_is_window_closed():
