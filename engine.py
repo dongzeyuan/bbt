2956f72645dadd0a73f30477c993a47867e090d4
dongzeyuan <dongfujing88@gmail.com>
2018-10-19 10:19:57 +0800

    Part 4

--- engine.py
+++ engine.py
@@ -1,4 +1,5 @@
 import tcod
+from fov_funcitons import initialize_fov, recompute_fov
 from entity import Entity
 from input_handlers import handle_keys
 from map_objects.game_map import GameMap
@@ -16,23 +17,37 @@ def main():
     room_min_size = 6
     max_rooms = 50
 
+    fov_algorithm = 5
+    fov_light_walls = True
+    fov_radius = 10
+
     colors = {
-        'dark_wall': tcod.Color(128, 128, 128),
-        'dark_ground': tcod.Color(242, 235, 190)
+        'dark_wall': tcod.Color(0, 0, 100),
+        'dark_ground': tcod.Color(50, 50, 150),
+        'light_wall': tcod.Color(130, 110, 50),
+        'light_ground': tcod.Color(200, 180, 50)
     }
 
-    player = Entity(int(screen_width / 2), int(screen_height / 2), '@', tcod.black)
-    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), '@', tcod.yellow)
+    player = Entity(int(screen_width / 2),
+                    int(screen_height / 2), '@', tcod.black)
+    npc = Entity(int(screen_width / 2 - 5),
+                 int(screen_height / 2), '@', tcod.yellow)
 
     entities = [player, npc]
 
-    tcod.console_set_custom_font("BBT\\fonts\\arial10x10.png", tcod.FONT_TYPE_GRAYSCALE | tcod.FONT_LAYOUT_TCOD)
+    tcod.console_set_custom_font(
+        "BBT\\fonts\\arial10x10.png", tcod.FONT_TYPE_GRAYSCALE | tcod.FONT_LAYOUT_TCOD)
     tcod.console_init_root(screen_width, screen_height, 'Engine', False)
 
     con = tcod.console_new(screen_width, screen_height)
 
     game_map = GameMap(map_width, map_height)
-    game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player)
+    game_map.make_map(max_rooms, room_min_size, room_max_size,
+                      map_width, map_height, player)
+
+    fov_recompute = True
+
+    fov_map = initialize_fov(game_map)
 
     key = tcod.Key()
     mouse = tcod.Mouse()
@@ -42,7 +57,14 @@ def main():
 
         tcod.sys_check_for_event(tcod.EVENT_KEY_PRESS, key, mouse)
 
-        render_all(con, entities, game_map, screen_width, screen_height, colors)
+        if fov_recompute:
+            recompute_fov(fov_map, player.x, player.y, fov_radius,
+                          fov_light_walls, fov_algorithm)
+
+        render_all(con, entities, game_map, fov_map, fov_recompute,
+                   screen_width, screen_height, colors)
+
+        fov_recompute = False
 
         tcod.console_flush()
 
@@ -58,6 +80,8 @@ def main():
             dx, dy = move
             if not game_map.is_blocked(player.x + dx, player.y + dy):
                 player.move(dx, dy)
+
+                fov_recompute = True
 
         if exit:
             return True
