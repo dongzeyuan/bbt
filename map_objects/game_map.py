dca6186782516ffa6b3ea2524405b1494bdee111
dongzeyuan <dongfujing88@gmail.com>
2018-10-22 15:58:52 +0800

    Part 6

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -1,9 +1,15 @@
 from random import randint
 import tcod
 
+from components.ai import BasicMonster
+from components.fighter import Fighter
+
 from entity import Entity
+
 from map_objects.rectangle import Rect
 from map_objects.tile import Tile
+
+from render_functions import RenderOrder
 
 
 class GameMap:
@@ -96,16 +102,21 @@ class GameMap:
         for i in range(number_of_monster):
             # Choose a random location in the room
             x = randint(room.x1 + 1, room.x2 - 1)
-            y = randint(room.y1+1, room.y2 - 1)
+            y = randint(room.y1 + 1, room.y2 - 1)
 
             if not any([entity for entity in entities if entity.x == x and entity.y == y]):
                 if randint(0, 100) < 80:
+                    fighter_component = Fighter(hp=10, defense=0, power=3)
+                    ai_component = BasicMonster()
                     monster = Entity(
-                        x, y, 'o', tcod.desaturated_green, 'Orc', blocks=True)
+                        x, y, 'o', tcod.desaturated_green, 'Orc', blocks=True, render_order=RenderOrder.ACTOR, fighter=fighter_component, ai=ai_component)
 
                 else:
+                    fighter_component = Fighter(hp=16, defense=1, power=4)
+                    ai_component = BasicMonster()
+
                     monster = Entity(
-                        x, y, 'T', tcod.darker_green, 'Troll', blocks=True)
+                        x, y, 'T', tcod.darker_green, 'Troll', blocks=True, fighter=fighter_component, render_order=RenderOrder.ACTOR, ai=ai_component)
 
                 entities.append(monster)
 
